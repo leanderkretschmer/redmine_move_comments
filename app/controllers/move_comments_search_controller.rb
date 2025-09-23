c9582ccd8f1a522c4b72506e7c487c5f6de7132f
leanderkretschmer <leanderkretschmer@icloud.com>
2025-09-23 15:31:58 +0200

    better search

--- app/controllers/move_comments_search_controller.rb
+++ app/controllers/move_comments_search_controller.rb
@@ -5,13 +5,24 @@ class MoveCommentsSearchController < ApplicationController
     term = params[:term].to_s.strip
     return render json: [] if term.blank?
 
-    # Simple subject search (case-insensitive)
-    issues = Issue.where("LOWER(#{Issue.table_name}.subject) LIKE LOWER(?)", "%#{term}%")
+    query = Issue.visible(User.current)
+
+    # Support Suche nach ID ("499" oder "#499") und nach Titel
+    if term.start_with?('#') && term[1..-1].to_s =~ /^\d+$/
+      id_num = term[1..-1].to_i
+      query = query.where(id: id_num)
+    elsif term =~ /^\d+$/
+      query = query.where(id: term.to_i)
+    else
+      query = query.where("LOWER(#{Issue.table_name}.subject) LIKE LOWER(?)", "%#{term}%")
+    end
+
+    issues = query.joins(:project)
                   .order(:id)
                   .limit(15)
-                  .pluck(:id, :subject)
+                  .pluck(:id, :subject, 'projects.name')
 
-    render json: issues.map { |id, subject| { id: id, subject: subject } }
+    render json: issues.map { |id, subject, project| { id: id, subject: subject, project: project } }
   end
 end
 
